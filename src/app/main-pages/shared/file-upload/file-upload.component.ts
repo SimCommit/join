ce73bb1d6be5ed928ed83918a91ad726ad9aba9b
SimCommit <168715756+SimCommit@users.noreply.github.com>
2025-11-09 15:01:34 +0100

    feat: add basic version of uploaded files preview to FileUploadComponent

--- src/app/main-pages/shared/file-upload/file-upload.component.ts
+++ src/app/main-pages/shared/file-upload/file-upload.component.ts
@@ -1,18 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-file-upload',
   imports: [],
   templateUrl: './file-upload.component.html',
-  styleUrl: './file-upload.component.scss'
+  styleUrl: './file-upload.component.scss',
 })
 export class FileUploadComponent {
+  // #region Properties
+  @ViewChild('filepicker') filepickerRef!: ElementRef<HTMLInputElement>;
+  // @ViewChild('previewGallery') previewGalleryRef!: ElementRef<HTMLInputElement>;
 
   thereAreUploads: boolean = true;
 
-  images: {"title": string, "URL": string}[] = [
-    {"title":"Title Img 1", "URL": ""},
-    {"title":"Title Img 2", "URL": ""},
+  images: { title: string; URL: string }[] = [
+    { title: 'Title Img 1', URL: '' },
   ];
+  // #endregion
 
+  constructor() {}
+
+  // #region Lifecycle
+  async ngAfterViewInit(): Promise<void> {
+    await this.initFilepickerListener();
+  }
+  // #endregion
+
+  // #region Eventlistener
+  async initFilepickerListener() {
+    const filepicker = this.filepickerRef.nativeElement;
+    filepicker.addEventListener('change', async () => {
+      const files = filepicker.files;
+      if (files!.length > 0) {
+        Array.from(files!).forEach(async (file) => {
+          const blob = new Blob([file], { type: file.type });
+          console.log('neue Datei: ', blob);
+
+          // const img = document.createElement("img");
+          // img.src = URL.createObjectURL(blob);
+
+          const url = URL.createObjectURL(blob);
+          this.images.push({title:"Test", URL: url})          
+        })
+      }
+    });
+  }
+  // #endregion
 }
